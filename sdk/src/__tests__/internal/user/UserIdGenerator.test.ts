e0cbc2f7f505d03bbd5e9ba22e6cefe677b8eb6b
kamospertsyan <spertsiankamo@gmail.com>
2022-09-30 09:33:24 +0300

    Moved tests to src folder

---
+++ sdk/src/__tests__/internal/user/UserIdGenerator.test.ts
@@ -0,0 +1,37 @@
+import {UserIdGenerator, UserIdGeneratorImpl} from '../../../internal/user';
+import {USER_ID_PREFIX, USER_ID_SEPARATOR} from '../../../internal/user/constants';
+
+const testUuid = 'b431fcbe-b067-4be0-9288-4a19887522e8';
+
+jest.mock('uuid', () => {
+  const originalModule = jest.requireActual('uuid');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    v4: jest.fn(() => testUuid),
+  };
+});
+
+import {v4 as uuidGenerator} from 'uuid';
+
+describe('UserIdGenerator tests', function () {
+  let userIdGenerator: UserIdGenerator;
+
+  beforeEach(() => {
+    userIdGenerator = new UserIdGeneratorImpl();
+  });
+
+  test('generate user id', () => {
+    // given
+    const uuidWithoutDashes = 'b431fcbeb0674be092884a19887522e8';
+    const expRes = `${USER_ID_PREFIX}${USER_ID_SEPARATOR}${uuidWithoutDashes}`;
+
+    // when
+    const res = userIdGenerator.generate();
+
+    // then
+    expect(res).toBe(expRes);
+    expect(uuidGenerator).toBeCalled();
+  });
+});
---
+++ sdk/src/__tests__/utils.ts
@@ -0,0 +1,12 @@
+import {QonversionError, QonversionErrorCode} from '../index';
+
+export function expectQonversionError(code: QonversionErrorCode, method: () => unknown) {
+  try {
+    method();
+    fail("Exception expected but was not thrown");
+  } catch (e) {
+    expect(e).toBeInstanceOf(QonversionError);
+    // @ts-ignore
+    expect(e.code).toBe(code);
+  }
+}
