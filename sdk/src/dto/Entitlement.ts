3ca1120ad9235104671eb16206654c5b17775d03
GitHub Action <action@github.com>
2023-05-02 12:50:22 +0300

    Entitlement type fixes

--- sdk/src/dto/Entitlement.ts
+++ sdk/src/dto/Entitlement.ts
@@ -1,38 +1,38 @@
 export type Entitlement = {
-   id: string;
-   active: boolean;
-   started: number;
-   expires: number;
-   source: EntitlementSource;
-   product?: Product;
+  id: string;
+  active: boolean;
+  started: number;
+  expires?: number | null;
+  source: EntitlementSource;
+  product?: Product;
 }
 
 export type Product = {
-   productId: string;
-   subscription?: Subscription;
+  productId: string;
+  subscription?: Subscription;
 };
 
 export type Subscription = {
-   renewState: RenewState;
-   currentPeriodType: PeriodType;
+  renewState: RenewState;
+  currentPeriodType: PeriodType;
 };
 
 export enum RenewState {
-   WillRenew = 'will_renew',
-   Canceled = 'canceled',
-   BillingIssue = 'billing_issue',
+  WillRenew = 'will_renew',
+  Canceled = 'canceled',
+  BillingIssue = 'billing_issue',
 }
 
 export enum PeriodType {
-   Normal = 'normal',
-   Trial = 'trial',
-   Intro = 'intro',
+  Normal = 'normal',
+  Trial = 'trial',
+  Intro = 'intro',
 }
 
 export enum EntitlementSource {
-   Unknown = 'unknown',
-   AppStore = 'appStore',
-   PlayStore = 'playStore',
-   Stripe = 'stripe',
-   Manual = 'manual'
+  Unknown = 'unknown',
+  AppStore = 'appstore',
+  PlayStore = 'playstore',
+  Stripe = 'stripe',
+  Manual = 'manual'
 }
