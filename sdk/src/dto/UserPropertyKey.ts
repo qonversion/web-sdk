9f9be86427d11ecdc1406c357614e0e7468db972
Kamo Spertsyan <spertsiankamo@gmail.com>
2024-01-30 18:14:58 +0300

    Added properties for PushWoosh and AppMetrica integration (#68)

--- sdk/src/dto/UserPropertyKey.ts
+++ sdk/src/dto/UserPropertyKey.ts
@@ -11,9 +11,13 @@ export enum UserPropertyKey {
     AppsFlyerUserId = "_q_appsflyer_user_id",
     AdjustAdId = "_q_adjust_adid",
     CustomUserId = "_q_custom_user_id",
-    FacebookAttribution = "_q_fb_attribution",
+    FacebookAttribution = "_q_fb_attribution", // Android only
     FirebaseAppInstanceId = "_q_firebase_instance_id",
-    AppSetId = "_q_app_set_id",
-    AdvertisingId = "_q_advertising_id",
+    AppSetId = "_q_app_set_id", // Android only
+    AdvertisingId = "_q_advertising_id", // iOS only
+    AppMetricaDeviceId = "_q_appmetrica_device_id",
+    AppMetricaUserProfileId = "_q_appmetrica_user_profile_id",
+    PushWooshHwId = "_q_pushwoosh_hwid",
+    PushWooshUserId = "_q_pushwoosh_user_id",
     Custom = "",
 }
