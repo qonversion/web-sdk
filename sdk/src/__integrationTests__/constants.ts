96091c3d77a7d688861cca087ab58d88a8e7ff8d
Kamo Spertsyan <spertsiankamo@gmail.com>
2025-08-25 00:30:16 +0300

    Move stripe constants to secrets.

--- sdk/src/__integrationTests__/constants.ts
+++ sdk/src/__integrationTests__/constants.ts
@@ -1,4 +1,11 @@
-export const AEGIS_URL = '<paste aegis link here>';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+export const AEGIS_URL = process.env.AEGIS_URL || '';
+export const STRIPE_PRODUCT_ID = process.env.STRIPE_PRODUCT_ID || '';
+export const STRIPE_SUBSCRIPTION_ID = process.env.STRIPE_SUBSCRIPTION_ID || '';
 export const PROJECT_KEY_FOR_TESTS = 'PV77YHL7qnGvsdmpTs7gimsxUvY-Znl2';
 export const PRIVATE_TOKEN_FOR_TESTS = 'sk_Yp-HzIBFO_mZE0YqNBeQLDrQoWmvJvSt';
 export const TS_EPSILON = 100;
